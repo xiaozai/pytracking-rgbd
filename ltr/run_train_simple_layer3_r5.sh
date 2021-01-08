d2893672f83fdb0c5cac273f08a8466d2c82e33d
xiaozai <ysongonly@gmail.com>
2021-01-08 15:12:01 +0200

    try roi=5x5

---
+++ ltr/run_train_simple_layer3_r5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=TT-SL3R5
+#SBATCH --output=/home/yans/pytracking-rgbd/ltr/logs/log-simple-layer3-roi5-output.txt
+#SBATCH --error=/home/yans/pytracking-rgbd/ltr/logs/log-simple-layer3-roi5-error.txt
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=2
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=64000
+#SBATCH --partition=gpu --gres=gpu:teslav100:1
+
+module load CUDA/10.0
+module load fgci-common
+module load ninja/1.9.0
+module load all/libjpeg-turbo/2.0.0-GCCcore-7.3.0
+
+source activate pytracking
+
+python run_training.py transformer transformer50_simple_layer3_r5
+
+conda deactivate
