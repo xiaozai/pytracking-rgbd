20b2d5527097f10611f6a117e6fffe3ad782133f
xiaozai <ysongonly@gmail.com>
2021-03-26 12:14:57 +0200

    fix load_and_save

--- pytracking/load_and_save_checkpoints.py
+++ pytracking/load_and_save_checkpoints.py
@@ -4,14 +4,16 @@ sys.path.append('/home/sgn/Data1/yan/pytracking-rgbd/pytracking')
 sys.path.append('/home/sgn/Data1/yan/pytracking-rgbd/ltr')
 
 from ltr.admin.loading import torch_load_legacy
+import torch
 
 if __name__ == '__main__':
 
 
     ''' We train networks one the Machince with Torch 1.7.1, but we want to test on torch 1.4.0 '''
-    
-    net_path = '/home/sgn/Data1/yan/pytracking-models/checkpoints/ltr/dimp/DeT_DiMP50_DO/ep0050.pth.tar'
+
+    net_path = '/home/sgn/Data1/yan/pytracking-models/checkpoints/ltr/bbreg/DeT_ATOM_Mean/ep0080.pth.tar'
     checkpoints = torch_load_legacy(net_path)
     # for key in checkpoints:
     #     print(key)
-    torch.save(checkpoints, '/home/sgn/Data1/yan/pytracking-models/checkpoints/ltr/dimp/DeT_DiMP50_DO/DeT_DiMP50_DO.pth.tar', _use_new_zipfile_serialization=False)
+    checkpoints['constructor'].fun_name = 'atom_resnet18_DeT'
+    torch.save(checkpoints, '/home/sgn/Data1/yan/pytracking-models/checkpoints/ltr/bbreg/DeT_ATOM_Mean/DeT_ATOM_Mean.pth.tar', _use_new_zipfile_serialization=False)
